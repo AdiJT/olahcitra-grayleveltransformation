7bddd0263cb5445fd1b8d9bc1baefb69a62e1115
Adi Juanito Taklal <aditaklal@gmail.com>
2024-02-09 09:59:08 +0800

    Tambah form options untuk Piece wise, Gray Level Split, Bit plane slicing, dan tambah form hasil untuk bit plane slicing dari bit 0 - 7. Tambah RBG Split yang memisah gambar menjadi komponen r, g, dan b nya beserta histogramnya ditamplikan di form hasilnya

---
+++ OlahCitra/Strategy/Log/LogStrategy.cs
@@ -0,0 +1,26 @@
+﻿using OlahCitra.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OlahCitra.Strategy
+{
+    public class LogStrategy : TransformationStrategy
+    {
+        public LogStrategy(FormOlahCitra olahCitraForm) : base(olahCitraForm)
+        {
+        }
+
+        public override Bitmap Transform()
+        {
+            if (_olahCitraForm.GrayScaleImage == null)
+                throw new Exception("Greyscale belum ada!");
+
+            var bitmap = _olahCitraForm.GrayScaleImage;
+            return ImageProcessing.GrayLevelTransformation(bitmap, TransformationFactory.Log(bitmap));
+        }
+    }
+}
